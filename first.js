db8926bd0d1cfc0fbcdae1c3fd5308210b449fc0
Bhavsar Aman <163817922+akak008@users.noreply.github.com>
2024-06-03 12:13:17 +0530

    Add files via upload

---
+++ first.js
@@ -0,0 +1,25 @@
+let searchform = document.querySelector('.search-form');
+document.querySelector('#search-btn').onclick = () => {
+    searchform.classList.toggle('active');
+    navbar.classList.remove('active');
+    cartItem.classList.remove('active');
+}
+
+let navbar = document.querySelector('.navbar');
+document.querySelector('#menu-btn').onclick = () => {
+    navbar.classList.toggle('active');
+    searchform.classList.remove('active');
+    cartItem.classList.remove('active');
+}
+
+let cartItem = document.querySelector('.cart-item-container');
+document.querySelector('#cart-btn').onclick = () => {
+    cartItem.classList.toggle('active');
+    navbar.classList.remove('active');
+    searchform.classList.remove('active');
+}
+window.onscroll = () =>{
+    navbar.classList.remove('active');
+    searchform.classList.remove('active');
+    cartItem.classList.remove('active');
+}
